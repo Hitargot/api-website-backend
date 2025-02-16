df72ba712519cdb02fd7415c8e57447b835ab7a2
Omar <omarlareef51@gmail.com>
2025-02-16 20:46:55 +0800

    using git for heroku deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-dotenv.config();
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    
     console.log("MongoDB connected...");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
